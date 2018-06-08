9e4e396b092fa687420d3299b15fe7d335cf56e0
masb80 <masb80@mun.ca>
2018-06-08 14:50:19 -0230

    reading frame from video

--- RealTimeImageProcessing/redObjectDetection/redObjectTracker.cxx
+++ RealTimeImageProcessing/redObjectDetection/redObjectTracker.cxx
@@ -7,9 +7,28 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-	namedWindow("Control Color", CV_WINDOW_AUTOSIZE); // taking a window for cntroling color 
-	 
+	namedWindow("Color_Control", CV_WINDOW_AUTOSIZE); // taking a window for cntroling color 
+	// HSV(Hue, saturation, value) color conversion
+	int iLowH = 0;
+	int iHighH = 179;
+	
+	int iLowS = 0;
+	int iHighS = 255;
+	
+	int iLowV = 0;
+	int iHighV = 255;
+	
+	cvCreateTrackbar("LowH","Color_Control", &iLowH, 179);
+	cvCreateTrackbar("HighH","Color_Control", &iHighH, 179);
+
+
+	
 	VideoCapture cap(0);
+	if(!cap.isOpened())
+	{
+		cout << " Can not open the video or webcam" << endl;
+		return -1;
+	}
 	Mat imgTemp;
 	//cap.read(imgTemp); // start a video temporary
 
@@ -17,6 +36,17 @@ int main(int argc, char** argv)
 
 	while(true) // eventing start
 	{
+		Mat imageOriginal;
+		
+		
+		bool newFrameFromWindow = cap.read(imageOriginal);
+		if(!newFrameFromWindow)
+		{
+			cout << "Can not read frame from video" << endl;
+		}
+		
+		cout << imageOriginal.size() << endl;
+		
 		if(waitKey(30)==27)
 		{
 			cout << "esc key pressed" << endl;
